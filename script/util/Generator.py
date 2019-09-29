bd0c0476844e2c019a4666910138bc0b58623c03
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-09-30 03:19:54 +0500

    database generated

--- script/util/Generator.py
+++ script/util/Generator.py
@@ -1,13 +1,28 @@
 
 def main():
     file = open('output.txt', 'w')
-    write(file, 1, 33, 1)
-    write(file, 33, 65, 2)
-    write(file, 65, 89, 3)
-    write(file, 89, 95, 4)
-    write(file, 95, 101, 5)
-    write(file, 101, 103, 6)
-    write(file, 103, 105, 7)
+    # input_file = open("input.txt", 'r')
+    # write1(input_file, file);
+
+    for i in range(0, 8):
+        if i % 2 == 0:
+            start = i * 8 + 1;
+            write(file, start, start + 8, 1) # A4
+        elif i % 2 != 0:
+            start = i * 8 + 1;
+            write(file, start, start + 8, 2)
+    # write(file, 1, 2, 1)
+    # write(file, 1, 2, 1)
+    # write(file, 1, 2, 1)
+    # write(file, 1, 2, 1)
+    # write(file, 1, 2, 1)
+    # write(file, 1, 2, 1)
+    # write(file, 17, 33, 4)
+    # write(file, 33, 49, 1)
+    # write(file, 49, 65, 4)
+    # write(file, 65, 73, 1)
+    # write(file, 73, 81, 2)
+    # write(file, 81, 89, 3)
 
 def write(file, start, end, static_id):
     for i in range(start, end):
@@ -18,9 +33,13 @@ def write(file, start, end, static_id):
 
 def write1(input_file, output_file):
     line = input_file.readline()
+    i = 0
     while line:
+        i = i + 1
         line = line.rstrip()
-        string = '(' + line  + '),\r'
+        string = '(' + line  + '), '
+        if i % 10 == 0:
+            string = '(' + line  + '),\r'
         output_file.write(string)
         line = input_file.readline()
 
